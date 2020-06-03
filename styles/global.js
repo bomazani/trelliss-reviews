d2bb329b0de97d35aebc6d71f4dfb773d0fcf798
Bob Hunt <bob.hunt.email@gmail.com>
2020-06-03 00:12:46 -0400

    Created a styles folder with a global.js file. Populated that file with the stlyes that are used globally in home.js, about.js & reviewDetails.js, and removed those styles from their original files. In App.js, imported {globalStyles} from '../styles/global'.

---
+++ styles/global.js
@@ -0,0 +1,21 @@
+import { StyleSheet } from 'react-native';
+
+export const globalStyles = StyleSheet.create({
+
+    container: {
+        flex: 1,
+        padding: 34,
+    },
+
+    titleText: {
+        fontFamily: 'nunito-bold',
+        fontSize: 18,
+        color:'#333',
+
+    },
+
+    paragraph: {
+        margingVertical: 8,
+        lineHeight: 20,
+    }
+});
