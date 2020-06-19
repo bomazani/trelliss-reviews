e708c587efc0713a3e794e0a4ead33c0ae7a6821
Bob Hunt <bob.hunt.email@gmail.com>
2020-06-18 21:21:02 -0400

    In app.json, updated the expo:name & slug from 'trelliss-zone' to 'trelliss-reviews'. Changes were made in app.json & homeStack.js in an attempt to create multipage navigation (unsuccessfully).

--- App.js
+++ App.js
@@ -1,35 +1,69 @@
-import React, {useState} from 'react';
+// *** vvv Original vvv
+// import React, {useState} from 'react';
+
+// *** vvv From comments https://quicksilvervideo.info/goon/react-native/mYeZgcuuZ913rr0 vvv
+import React, { useState } from 'react';
 import * as Font from 'expo-font';
+import{ AppLoading } from 'expo';
+import Navigator from "./routes/homeStack";
 
-import Home from './screens/home';
-import ReviewDetails from './screens/reviewDetails';
-import About from './screens/about';
-
-import { AppLoading } from 'expo';
-import Navigator from './routes/homeStack';
+// *** vvv Original vvv
+// import * as Font from 'expo-font';
+// import Home from './screens/home';
+// import ReviewDetails from './screens/reviewDetails';
+// import About from './screens/about';
+// import { AppLoading } from 'expo';
+// import Navigator from './routes/homeStack';
 // import HomeStack from './routes/homeStack';
 
-
+// *** vvv From comments https://quicksilvervideo.info/goon/react-native/mYeZgcuuZ913rr0 vvv
 const getFonts = () => Font.loadAsync({
+  // 'roboto-regular' : require('./assets/fonts/Roboto-Regular.ttf'),
+  // 'roboto-bold' : require('./assets/fonts/Roboto-Bold.ttf')
+  // Original font packages (next two lines)
   'nunito-regular': require('./assets/fonts/Nunito-Regular.ttf'),
   'nunito-bold': require('./assets/fonts/Nunito-Bold.ttf'),
 });
 
 export default function App() {
-  const [fontsLoaded, setFontsLoaded] = useState(false);
-
-  if(fontsLoaded){
+  const [fontLoaded, setFontLoaded] = useState(false);
+  
+  if(fontLoaded){
     return (
-      // <Home />
-      // <Navigator />
-      <HomeStack />
+      Navigator()
     );
   } else {
-    return (
+    return(
       <AppLoading
         startAsync={getFonts}
-        onFinish={()=> setFontsLoaded(true)}
-    />
+        onFinish={()=> setFontLoaded(true)}
+      />
     )
   }
 }
+
+
+// *** vvv Original vvv
+// const getFonts = () => Font.loadAsync({
+  // 'nunito-regular': require('./assets/fonts/Nunito-Regular.ttf'),
+  // 'nunito-bold': require('./assets/fonts/Nunito-Bold.ttf'),
+// });
+
+// export default function App() {
+//   const [fontsLoaded, setFontsLoaded] = useState(false);
+
+//   if(fontsLoaded){
+//     return (
+//       // <Home />
+//       // <Navigator />
+//       <HomeStack />
+//     );
+//   } else {
+//     return (
+//       <AppLoading
+//         startAsync={getFonts}
+//         onFinish={()=> setFontsLoaded(true)}
+//       />
+//     )
+//   }
+// }
--- routes/homeStack.js
+++ routes/homeStack.js
@@ -1,19 +1,66 @@
-import { createStackNavigator } from 'react-navigation-stack';
+// *** vvv From comments https://quicksilvervideo.info/goon/react-native/mYeZgcuuZ913rr0 vvv
+import {NavigationContainer, StackActions} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import Home from "../screens/home";
+import ReviewDetails from "../screens/reviewDetails";
+
+// *** vvv Original vvv
+// import { createStackNavigator } from 'react-navigation-stack';
 // import { createStackNavigator } from 'react-navigation/stack';
 // import { createAppContainer } from 'react-navigation';
-import { createAppContainer } from '@react-navigation/native';
+// import { createAppContainer } from '@react-navigation/native';
+// import Home from '../screens/home';
+// import ReviewDetails from '../screens/reviewDetails';
 
-import Home from '../screens/home';
-import ReviewDetails from '../screens/reviewDetails';
 
+// const screens = {
+//     Home: {
+//         screen: Home
+//     },
+//     ReviewDetails: {
+//         screen: ReviewDetails
+//     },
+// }
 
-const screens = {
-    Home: {
-        screen: Home
-    },
-    ReviewDetails: {
-        screen: ReviewDetails
-    },
+// *** vvv From comments https://quicksilvervideo.info/goon/react-native/mYeZgcuuZ913rr0 vvv
+// const Stack = createStackNavigator() 
+//     export default function Navigator() { 
+//         return ();
+//     };
+
+// *** vvv Original code vvv
+// const HomeStack = createStackNavigator(screens);
+// export default createAppContainer(HomeStack);
+const HomeStack = createStackNavigator();
+
+function RootStack() {
+    return (
+        <StackActions.createStackNavigator
+            initialRouteName="Home"
+            screenOptions={{ gestureEnabled: false }}
+        >
+            <Stack.screen
+                name="Home"
+                component={Home}
+                options={{ title: 'My app'}}
+            />
+            <Stack.Screen
+                name="ReviewDetails"
+                component={ReviewDetails}
+            />
+        </StackActions.createStackNavigator>
+    );
 }
+    // {
+    //     Home: {
+    //         screen: Home,
+    //     },
+    //     ReviewDetails: {
+    //         screen: ReviewDetails,
+    //     },
+    // },
 
-const HomeStack = createStackNavigator(screens);
+// )
+export default function App() {
+    return <NavigationContainer>{ /*...*/ }</NavigationContainer>
+};
